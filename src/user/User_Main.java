ff46499498e9d516b49eb0c031da225801ae9c86
Bu-Sung <qntjd4680@gmail.com>
2022-05-16 23:09:00 +0900

    병합 오류 해결

--- src/user/User_Main.java
+++ src/user/User_Main.java
@@ -87,6 +87,11 @@ public class User_Main extends javax.swing.JFrame {
         jScrollPane1.setViewportView(TABLE);
 
         jButton1.setText("계좌 개설 하기");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         event.setText("공지사항 확인");
 
@@ -138,6 +143,10 @@ public class User_Main extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
