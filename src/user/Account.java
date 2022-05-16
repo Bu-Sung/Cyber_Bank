4e7073f29f3dfd519d693b6e0942931a93d5e17a
Bu-Sung <qntjd4680@gmail.com>
2022-05-17 03:20:08 +0900

    DB 저장 순서 변경

--- src/user/Account.java
+++ src/user/Account.java
@@ -2,20 +2,9 @@
 package user;
 
 abstract class Account {
-    
-  public String owner;
-
-  protected int balance;
-
-  protected String accountNumber;
-  
-  protected Account_Type account_Type;
-
-  protected With_Or_Without_Card with_Or_Without_Card;
-
-
-  
-  abstract String benefits();
-  
-  abstract void create_Account(String Id, String Pw, String Kind);
+    protected String accountNumber;
+    protected Account_Type account_Type;
+    protected With_Or_Without_Card with_Or_Without_Card;
+    abstract String benefits();
+    abstract void create_Account(String Id, String Pw);
 }
